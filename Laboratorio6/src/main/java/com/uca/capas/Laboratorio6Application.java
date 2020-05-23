27367d73fffdc8cc2b17b4cf554816fc7cae7cb9
vegadevh <00148816@uca.edu.sv>
2020-05-23 12:31:22 -0600

    Proyecto base

---
+++ Laboratorio6/src/main/java/com/uca/capas/Laboratorio6Application.java
@@ -0,0 +1,13 @@
+package com.uca.capas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratorio6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laboratorio6Application.class, args);
+	}
+
+}
